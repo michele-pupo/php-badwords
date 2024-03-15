d42c2dd5221a1d95a5edf342b3589ab2e0ffbe6e
michele-pupo <mikypupo@hotmail.it>
2024-03-15 14:21:16 +0100

    added paragraph insertion input

--- index.php
+++ index.php
@@ -5,15 +5,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Badwords</title>
 
-     <!-- fontawesome -->
-     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
     <!-- personal style  -->
     <link rel="stylesheet" href="css/style.css">
 
 </head>
 <body>
-    <h1>Taboolean</h1>
+    <div class="d-flex justify-content-center">
+        <h1 class="display-1 fw-bold pt-3">Taboolean</h1>
+    </div>
+    <div class="container">
+        <form action="response.php" methods="GET">
+            <!-- input di inserimento paragrafo -->
+            <div>
+                <label for="paragraph" class="form-label"></label>
+                <textarea class="p-2" name="paragraph" id="paragraph" cols="20" rows="10" placeholder="inserisci il testo da modificare"></textarea>
+            </div>
+            <!-- fine input di inserimento paragrafo -->
+        </form>
+    </div>
+    
+
     <!-- script bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
